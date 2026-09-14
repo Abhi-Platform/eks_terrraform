7f3428a84be4d220fef45523e2364e2304330c43
Nashabhi <akottapalli9@gmail.com>
2026-09-14 11:44:00 -0500

    Initial Terraform configuration

---
+++ backend/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "us-east-2"
+}
+resource "aws_s3_bucket" "example" {
+  bucket = "demo-terraform-eks-states-bucket"
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  name           = "Terraform-eks-states-locks"
+  billing_mode   = "PAY_PER_REQUEST"
+    hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
